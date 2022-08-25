1564f15d103d70751ea5fc87a57a3bd2353abac5
uma shankar <umashankar2@example.com>
2022-08-25 07:50:38 +0000

    Adds dsl-jobs

---
+++ dsl-jobs/html_generator.groovy
@@ -0,0 +1,22 @@
+pipelineJob('html-generator') { {
+    parameters {
+        stringParam('DEPLOYED_BY',
+            defaultValue = '',
+            description = 'Identifies who generated the latest HTML.'
+        )
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote { 
+                        url('https://github.com/ricardoandre97/go-html-renderer.git') // Your Github repo
+                        credentials("go-html-renderer") // Credentials ID from the previous step
+                    }
+                    branches('dynamic-html')
+                    scriptPath('Jenkinsfile')
+                }
+            }
+        }
+    }
+}
